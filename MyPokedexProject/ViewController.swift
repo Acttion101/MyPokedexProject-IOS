d0de9ce191dbe0fc6cd7bdda6a68ec1e88ea9595
acttion101 <pongpon102@gmail.com>
2021-02-02 13:48:24 +0700

    Initial Commit

---
+++ MyPokedexProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPokedexProject
+//
+//  Created by pongpon on 2/2/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
